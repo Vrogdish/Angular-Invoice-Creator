0975d64b785f82ef4d5811740298c816b9c5991a
Vrogdish <vrogdish@outlook.com>
2024-05-29 19:00:36 +0200

    feat : homepage

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { BtnComponent } from "../../shared/components/btn/btn.component";
+import { BtnComponent } from '../../shared/components/btn/btn.component';
+import { AuthService } from '../../core/auth/services/auth.service';
+import { User } from 'firebase/auth';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    imports: [BtnComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [BtnComponent, CommonModule, RouterLink],
 })
 export class HomeComponent {
+  user!: User | null;
+  constructor(private auth: AuthService) {}
 
+  ngOnInit() {
+    this.auth.authState$.subscribe((user) => {
+      this.user = user;
+    });
+  }
 }
