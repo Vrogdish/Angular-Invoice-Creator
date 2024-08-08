2d5bba98f836073d1f68d44526ef61e3110fd73a
Vrogdish <vrogdish@outlook.com>
2024-08-08 17:20:57 +0200

    feat : legal

---
+++ src/app/features/legals/terms-of-service/terms-of-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-of-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './terms-of-service.component.html',
+  styleUrl: './terms-of-service.component.scss'
+})
+export class TermsOfServiceComponent {
+
+}
